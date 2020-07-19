22473d31bc4c620977c21b1aa1a6e479b4bddef7
pradnyesh45 <pradnyeshaglawe12@gmail.com>
2020-07-19 18:45:29 +0530

    React and Redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,87 +1,88 @@
 import React from 'react';
-import {data} from '../data';
+import { connect } from 'react-redux';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
 import { addMovies, setShowFavourites } from '../actions';
-import {StoreContext} from '../index';
+import { data as moviesList } from '../data';
 
 class App extends React.Component {
   componentDidMount() {
-    const {store} = this.props;
-    store.subscribe(() => {
-      console.log('UPDATED');
-      this.forceUpdate();
-    });
-    // make api call
-    // dispatch action
-    store.dispatch(addMovies(data));
-
-    console.log('STATE', this.props.store.getState());
+    this.props.dispatch(addMovies(moviesList));
   }
 
-  isMovieFavourite = (movie) => {
-    const {movies} = this.props.store.getState();
+  isMovieInFavourites = (movie) => {
+    const { movies } = this.props;
 
     const index = movies.favourites.indexOf(movie);
-
     if (index !== -1) {
-      // found the movie
       return true;
     }
+
     return false;
-  }
-  onChangeTab = (val) => {
-    this.props.store.dispatch(setShowFavourites(val));
-  }
+  };
+
+  changeTab = (val) => {
+    this.props.dispatch(setShowFavourites(val));
+  };
   render() {
-    const {movies, search} = this.props.store.getState(); // will return {movies: {}, search: []
+    const { movies, search } = this.props; // will return { movies: {}, search: []}
     console.log('movies', movies);
-    const {list, favourites, showFavorites} = movies; 
-    const displayMovies = showFavorites ? favourites : list;
-    
+    const { list, showFavourites = [], favourites = [] } = movies;
+    const displayMovies = showFavourites ? favourites : list;
+
     return (
       <div className="App">
         <Navbar search={search} />
         <div className="main">
           <div className="tabs">
-            <div className={`tab ${showFavorites ? '' : 'active-tabs'}`} onClick={() => this.onChangeTab(false)}>Movies</div>
-            <div className={`tab ${showFavorites ? 'active-tabs' : ''}`} onClick={() => this.onChangeTab(true)}>Favourites</div>
+            <div
+              className={`tab ${showFavourites ? '' : 'active-tabs'}`}
+              onClick={() => this.changeTab(false)}
+            >
+              Movies
+            </div>
+            <div
+              className={`tab ${showFavourites ? 'active-tabs' : ''}`}
+              onClick={() => this.changeTab(true)}
+            >
+              Favourites
+            </div>
           </div>
-      
-          <div className="list">
-            {displayMovies.map((movie, index) => (
-              <MovieCard 
+
+          <div id="list">
+            {displayMovies.map((movie) => (
+              <MovieCard
                 movie={movie}
-                key={`movies-${index}`}
-                dispatch={this.props.store.dispatch}
-                isFavourite={this.isMovieFavourite(movie)}
+                key={movie.imdbID}
+                dispatch={this.props.dispatch}
+                isFavourite={this.isMovieInFavourites(movie)}
               />
             ))}
+            {displayMovies.length === 0 ? (
+              <div className="no-movies">No movies to display! </div>
+            ) : null}
           </div>
-              {displayMovies.length === 0 ? <div className="no-movies">No movies to display!</div> : null}
         </div>
       </div>
     );
-
   }
 }
 
-class AppWrapper extends React.Component {
-  render() {
-    return (
-      <StoreContext.Consumer>
-        {(store) => <App store={store} />}
-      </StoreContext.Consumer>
-    );
-  }
-}
+// class AppWrapper extends React.Component {
+//   render() {
+//     return (
+//       <StoreContext.Consumer>
+//         {(store) => <App store={store} />}
+//       </StoreContext.Consumer>
+//     );
+//   }
+// }
 
-function callback (state) {
+function callback(state) {
   return {
     movies: state.movies,
-    search: state.movies
-  }
-};
-const connectedComponent = (callback)(App);
-
-export default AppWrapper;
+    search: state.movies,
+  };
+}
+const connectedComponent = connect(callback)(App);
+export default connectedComponent;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,75 +1,74 @@
-import React from 'react';
-// import {data} from '../data';
-import {addMovieToList, handleMovieSearch} from '../actions';
-import {StoreContext} from '../index';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addMovieToList, handleMovieSearch } from '../actions';
 
-class Navbar extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            searchText: ''
-        };
-    }
-
-    handleAddToMovies = (movie) => {
-        this.props.dispatch(addMovieToList(movie));
-        this.setState({
-            showSearchResults: false
-        });
-    }
-
-    handleSearch = () => {
-        const {searchText} = this.state;
-
-        this.props.dispatch(handleMovieSearch(searchText));
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
     };
+  }
+  handleAddToMovies = (movie) => {
+    this.props.dispatch(addMovieToList(movie));
+  };
 
-    handleChange = (e) => {
-        this.setState({
-            searchText: e.target.value
-        });
-    };
+  handleSearchClick = () => {
+    const { searchText } = this.state;
+    this.props.dispatch(handleMovieSearch(searchText));
+  };
 
-    render() {
-        const {result: movie, showSearchResults} = this.props.search;
-        return (
-            <div className="nav">
-                <div className="search-container">
-                    <input onChange={this.handleSearchChange} />
-                    <button id="search-btn" onClick={this.handleSearchClick}>
-                        Search
-                    </button>
-                
-                    {showSearchResults && 
-                        <div className="search-results">
-                            <div className="search-result"></div>
-                            <img src={movie.Poster} alt="search-pic" />
+  handleSearchChange = (e) => {
+    this.setState({
+      searchText: e.target.value,
+    });
+  };
 
-                            <div className="movie-info">
-                                <span>{movie.Title}</span>
-                                <button onClick={() => this.handleAddToMovies(movie)}>
-                                    Add to Movies
-                                </button>
-                            </div>
-                        </div>
-                    }
+  render() {
+    const { showSearchResults, results: movie } = this.props.search;
+    return (
+      <div className="nav">
+        <div className="search-container">
+          <input onChange={this.handleSearchChange} />
+          <button id="search-btn" onClick={this.handleSearchClick}>
+            Search
+          </button>
+
+          {showSearchResults && (
+            <div className="search-results">
+              <div className="search-result">
+                <img src={movie.Poster} alt="search-pic" />
+                <div className="movie-info">
+                  <span>{movie.Title}</span>
+                  <button onClick={() => this.handleAddToMovies(movie)}>
+                    Add to Movies
+                  </button>
                 </div>
+              </div>
             </div>
-        );
-    }
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
-class NavbarWrapper extends React.Component {
-    render() {
-        return (
-            <StoreContext.Consumer>
-                {(store) => (
-                    <Navbar dispatch={store.dispatch} search={this.props.search} />
-                )}
-            </StoreContext.Consumer>
-        );
-    }
+// class NavbarWrapper extends React.Component {
+//   render() {
+//     return (
+//       <StoreContext.Consumer>
+//         {(store) => (
+//           <Navbar dispatch={store.dispatch} search={this.props.search} />
+//         )}
+//       </StoreContext.Consumer>
+//     );
+//   }
+// }
+
+function mapStateToProps({ search }) {
+  return {
+    search,
+  };
 }
 
-export default NavbarWrapper;
+export default connect(mapStateToProps)(Navbar);
